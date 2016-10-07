c6caf81c247d8dc77b6b156362b09607944ff333
Divine <poriter.coco@gmail.com>
2016-10-07 10:06:06 +0800

    sock_merchant

---
+++ algorithms/implementation/sock_merchant_test.go
@@ -0,0 +1,9 @@
+package implementation
+
+import "testing"
+
+func TestSockMerchant(t *testing.T) {
+	if a := sock_merchant([]int{10, 20, 20, 10, 10, 30, 50, 10, 20}); a != 3 {
+		t.Errorf("%d\n", a)
+	}
+}
