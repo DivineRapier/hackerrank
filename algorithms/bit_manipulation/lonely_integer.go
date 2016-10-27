e8571566e9ff58c306dde2d4d9f458e084573564
Divine <poriter.coco@gmail.com>
2016-10-27 13:37:22 +0800

    add sum_vs_xor；0是异或的零元，1是异或的逆元

--- algorithms/bit_manipulation/lonely_integer.go
+++ algorithms/bit_manipulation/lonely_integer.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package bit_manipulation
 
 func lonely_integer(nums []int) int {
 	lnum := 0
@@ -8,15 +6,4 @@ func lonely_integer(nums []int) int {
 		lnum ^= v
 	}
 	return lnum
-}
-
-func main() {
-	//Enter your code here. Read input from STDIN. Print output to STDOUT
-	var n int
-	num := make([]int, n, n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &num[i])
-	}
-	a := lonely_integer(num)
-	fmt.Printf("%d\n", a)
 }
---
+++ algorithms/bit_manipulation/sum_vs_xor_test.go
@@ -0,0 +1,69 @@
+package bit_manipulation
+
+import "testing"
+
+// func TestSumVSXor(t *testing.T) {
+// 	if a := sum_vs_xor(5); a != 2 {
+// 		t.Errorf("expect %d but %d\n", 2, a)
+// 	}
+// 	if a := sum_vs_xor(10); a != 4 {
+// 		t.Errorf("expect %d but %d\n", 4, a)
+// 	}
+// }
+
+func TestZeroCount(t *testing.T) {
+	if a := zero_count(0); a != 0 {
+		t.Errorf("expect %d but %d\n", 0, a)
+	}
+	if a := zero_count(1); a != 0 {
+		t.Errorf("expect %d but %d\n", 0, a)
+	}
+	if a := zero_count(2); a != 1 {
+		t.Errorf("expect %d but %d\n", 1, a)
+	}
+	if a := zero_count(3); a != 0 {
+		t.Errorf("expect %d but %d\n", 0, a)
+	}
+	if a := zero_count(4); a != 2 {
+		t.Errorf("expect %d but %d\n", 2, a)
+	}
+	if a := zero_count(5); a != 1 {
+		t.Errorf("expect %d but %d\n", 1, a)
+	}
+}
+
+func TestSumXor(t *testing.T) {
+	if a := sum_vs_xor(0); a != 1 {
+		t.Errorf("expect %d but %d\n", 1, a)
+	}
+	if a := sum_vs_xor(1); a != 1 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(2); a != 2 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(3); a != 1 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(4); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(5); a != 2 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(6); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(7); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(8); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(9); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+	if a := sum_vs_xor(10); a != 4 {
+		t.Errorf("expect %d but %d\n", 4, a)
+	}
+}
